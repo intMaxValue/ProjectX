9a8ee5e3c28ef596417bb42ed54049cd2d3667a1
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 15:14:21 +0200

    Added Appointment

---
+++ ProjectX.Infrastructure/Data/Models/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectX.Infrastructure.Data.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        // Add properties for appointment details (e.g., date, time, service, etc.)
+
+        public DateTime DateAndTime { get; set; }
+
+        [ForeignKey(nameof(Salon))]
+        public int SalonId { get; set; } // Foreign key to Salon
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = string.Empty; // Foreign key to User
+
+        // Navigation properties
+        public Salon Salon { get; set; } = null!;
+        public User User { get; set; } = null!;
+    }
+}
