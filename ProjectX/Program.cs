b0179d9f4af89ad9769acac40e8ff978912ab255
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 13:44:24 +0200

    Added services in Program.cs

--- ProjectX/Program.cs
+++ ProjectX/Program.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using ProjectX.Infrastructure.Data;
+using ProjectX.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddApplicationIdentity(builder.Configuration);
+
+builder.Services.AddApplicationDbContext(builder.Configuration);
+
+builder.Services.AddApplicationServices();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
