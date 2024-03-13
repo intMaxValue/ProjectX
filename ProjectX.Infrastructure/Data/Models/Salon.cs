6dc44b2910643882fb24b8268ccdd8603bdc5f91
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 15:13:40 +0200

    Added Salon

---
+++ ProjectX.Infrastructure/Data/Models/Salon.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectX.Infrastructure.Data.Models
+{
+    public class Salon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string MapUrl { get; set; } = string.Empty;
+        public string OwnerId { get; set; } = string.Empty; // Foreign key to User
+
+        // Navigation properties
+        public User Owner { get; set; } = null!;
+        public ICollection<Photo> Photos { get; set; } = null!;
+        public ICollection<Review> Reviews { get; set; } = null!;
+        public ICollection<Appointment> Appointments { get; set; } = null!;
+        public ICollection<Availability> Availabilities { get; set; } = null!;
+    }
+}
