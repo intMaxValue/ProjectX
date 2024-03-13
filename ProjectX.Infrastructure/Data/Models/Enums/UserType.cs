429f4e5b9abc92e64f3f3016788a4fcb80c684c8
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 15:13:08 +0200

    Added Enums

---
+++ ProjectX.Infrastructure/Data/Models/Enums/UserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX.Infrastructure.Data.Models.Enums
+{
+    public enum UserType
+    {
+        User = 0,
+        SalonOwner = 1
+    }
+}
