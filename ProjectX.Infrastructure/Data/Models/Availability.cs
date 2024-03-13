e48db91e2bb9b9e44d929bb0e71584db4976ab05
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 15:14:09 +0200

    Added Availability

---
+++ ProjectX.Infrastructure/Data/Models/Availability.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectX.Infrastructure.Data.Models
+{
+    public class Availability
+    {
+        public int Id { get; set; }
+        // Add properties for availability details (e.g., day, time, etc.)
+
+        public DateTime DateAndTime { get; set; }
+        public int SalonId { get; set; } // Foreign key to Salon
+
+        // Navigation property
+        public Salon Salon { get; set; } = null!;
+    }
+}
