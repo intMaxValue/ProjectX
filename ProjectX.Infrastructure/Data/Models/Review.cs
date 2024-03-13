732c1cf4565a78abfb7319e8d922d7b9ca1f79e6
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 15:13:50 +0200

    Added Review

---
+++ ProjectX.Infrastructure/Data/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectX.Infrastructure.Data.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Salon))]
+        public int SalonId { get; set; } // Foreign key to Salon
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = string.Empty; // Foreign key to User
+        public string Comment { get; set; } = string.Empty;
+        public DateTime DatePosted { get; set; }
+
+        // Navigation properties
+        public Salon Salon { get; set; } = null!;
+        public User User { get; set; } = null!;
+    }
+}
