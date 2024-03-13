07fd739b3a76f2d539ed77be0fce7e10864c3b24
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-13 15:13:29 +0200

    Added User

---
+++ ProjectX.Infrastructure/Data/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectX.Infrastructure.Data.Models.Enums;
+
+namespace ProjectX.Infrastructure.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public UserType UserType { get; set; }
+        public string ProfilePicture { get; set; } = string.Empty;
+
+        // Navigation properties
+        public ICollection<Review> Reviews { get; set; } = null!;
+        public ICollection<Photo> Photos { get; set; } = null!;
+    }
+}
