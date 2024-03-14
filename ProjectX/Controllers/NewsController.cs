cf9fc97750a043990fa45eb69787bd35cc6419c7
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-14 18:14:24 +0200

    Added NewsController logic

--- ProjectX/Controllers/NewsController.cs
+++ ProjectX/Controllers/NewsController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectX.Core.Services;
 
 namespace ProjectX.Controllers
 {
+    
     public class NewsController : Controller
     {
-        public IActionResult Index()
+        private readonly RssFeedService _rssFeedService;
+
+        public NewsController(RssFeedService rssFeedService)
         {
-            return View();
+            _rssFeedService = rssFeedService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var rssFeedUrl = "https://www.nailsmag.com/rss"; // Replace this with your RSS feed URL
+            var newsItems = await _rssFeedService.GetRssFeedAsync(rssFeedUrl);
+            return View(newsItems);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LoadMoreArticles(int page)
+        {
+            int pageSize = 10;
+            var newsItems = await _rssFeedService.GetRssFeedAsync("https://www.nailsmag.com/rss", page, pageSize);
+
+            return PartialView("_NewsFeedPartial", newsItems);
         }
     }
 }
