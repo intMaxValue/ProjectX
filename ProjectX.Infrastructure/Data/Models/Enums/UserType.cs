fe0132ead28de0316a9391a07a275d5c12b9af8d
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-14 17:15:01 +0200

    Cleared UserType

--- ProjectX.Infrastructure/Data/Models/Enums/UserType.cs
+++ ProjectX.Infrastructure/Data/Models/Enums/UserType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectX.Infrastructure.Data.Models.Enums
+﻿namespace ProjectX.Infrastructure.Data.Models.Enums
 {
     public enum UserType
     {
