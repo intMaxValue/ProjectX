3a69f42f5723e0889a2a389da26474253650287d
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-14 18:16:28 +0200

    Added ViewModels class library

---
+++ ProjectX.ViewModels/RssNewsFeed/NewsItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectX.ViewModels.RssNewsFeed
+{
+    public class NewsItemViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public DateTime PublishDate { get; set; }
+        public string Creator { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
