b9988e93c2ac62ee29cfad8f6c98b98d705125ca
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-15 12:45:42 +0200

    Created ISalonService

---
+++ ProjectX.Core/Contracts/ISalonService.cs
@@ -0,0 +1,12 @@
+﻿using ProjectX.Infrastructure.Data.Models;
+
+namespace ProjectX.Core.Contracts
+{
+    public interface ISalonService
+    {
+        Task<IEnumerable<Salon>> GetAllSalonsAsync(string searchQuery);
+        Task<Salon?> GetSalonByIdAsync(int id);
+        Task<Salon> CreateSalonAsync(Salon salon);
+        Task DeleteSalonAsync(int id);
+    }
+}
