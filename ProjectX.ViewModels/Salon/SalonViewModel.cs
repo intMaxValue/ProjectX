ba902d55c2c75bd674ad8ea01d3eb981ab3a74ae
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-15 12:47:21 +0200

    Created SalonViewModel

---
+++ ProjectX.ViewModels/Salon/SalonViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectX.ViewModels.Salon
+{
+    public class SalonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string MapUrl { get; set; } = string.Empty;
+    }
+}
