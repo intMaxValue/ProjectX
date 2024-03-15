b9f59154d1cb58a0493281c0fe661412aed163f0
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-15 17:27:42 +0200

    Modified SalonsController

--- ProjectX/Controllers/SalonsController.cs
+++ ProjectX/Controllers/SalonsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using ProjectX.Core.Contracts;
+using ProjectX.Infrastructure.Data.Models;
 using ProjectX.ViewModels.Salon;
 
 namespace ProjectX.Controllers
@@ -7,10 +9,11 @@ namespace ProjectX.Controllers
     public class SalonsController : Controller
     {
         private readonly ISalonService _salonService;
-
-        public SalonsController(ISalonService salonService)
+        private readonly UserManager<IdentityUser> _userManager;
+        public SalonsController(ISalonService salonService, UserManager<IdentityUser> userManager)
         {
             _salonService = salonService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index(string searchQuery)
@@ -29,6 +32,37 @@ namespace ProjectX.Controllers
             });
 
             return View(salonViewModels);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+            string userId = currentUser.Id;
+            ViewBag.UserId = userId;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateSalonViewModel model)
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+            string userId = currentUser.Id;
+
+            if (ModelState.IsValid && userId != null)
+            {
+                try
+                {
+                    await _salonService.CreateSalonAsync(model, userId);
+                    return RedirectToAction("Index"); 
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "An error occurred while saving the salon.");
+                    return View(model);
+                }
+            }
+
+            return View(model);
         }
     }
 }
