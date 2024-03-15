7a9d3db7fa2b578c376c3852318fc0c8eb7f2021
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-15 12:44:34 +0200

    Added SalonController

---
+++ ProjectX/Controllers/SalonsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectX.Core.Contracts;
+using ProjectX.ViewModels.Salon;
+
+namespace ProjectX.Controllers
+{
+    public class SalonsController : Controller
+    {
+        private readonly ISalonService _salonService;
+
+        public SalonsController(ISalonService salonService)
+        {
+            _salonService = salonService;
+        }
+
+        public async Task<IActionResult> Index(string searchQuery)
+        {
+            var salons = await _salonService.GetAllSalonsAsync(searchQuery);
+
+            var salonViewModels = salons.Select(s => new SalonViewModel
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Address = s.Address,
+                City = s.City,
+                PhoneNumber = s.PhoneNumber,
+                Description = s.Description,
+                MapUrl = s.MapUrl
+            });
+
+            return View(salonViewModels);
+        }
+    }
+}
