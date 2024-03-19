bdf4027e63eb6b63347f39545aa61c79c7dd43d3
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-19 16:24:10 +0200

    Modified Salon model

--- ProjectX.Infrastructure/Data/Models/Salon.cs
+++ ProjectX.Infrastructure/Data/Models/Salon.cs
@@ -10,6 +10,7 @@
         public string Description { get; set; } = string.Empty;
         public string MapUrl { get; set; } = string.Empty;
         public string OwnerId { get; set; } = string.Empty; // Foreign key to User
+        public string ProfilePictureUrl { get; set; } = string.Empty;
 
         // Navigation properties
         public User Owner { get; set; } = null!;
