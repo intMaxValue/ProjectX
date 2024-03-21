3f531b9d1b4edbbb34fc01ce5437beee15fa94c4
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-21 13:36:29 +0200

    HomeController modified, only for unautthenticated users

--- ProjectX/Controllers/HomeController.cs
+++ ProjectX/Controllers/HomeController.cs
@@ -1,23 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using ProjectX.Models;
 using System.Diagnostics;
+using ProjectX.Infrastructure.Data.Models;
 
 namespace ProjectX.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
+        [AllowAnonymous] // Allow access to non-logged-in users
         public IActionResult Index()
         {
+            // Check if the user is authenticated
+            if (User?.Identity?.IsAuthenticated == true)
+            {
+                // If authenticated, redirect to the NewsController's Index action
+                return RedirectToAction("Index", "News");
+            }
+
             return View();
         }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
