e0ebb436bb236ec703d3d191f9eda86168dd2490
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-21 11:44:28 +0200

    Added CompleteProfile ViewModel

---
+++ ProjectX.ViewModels/Profile/CompleteProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectX.ViewModels.Profile
+{
+    public class CompleteProfileViewModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string ProfilePictureUrl { get; set; } = string.Empty;
+    }
+}
