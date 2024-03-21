7181d2b3a0d70c76faaae76e8e7b1e33877230a8
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-21 11:53:54 +0200

    Added redirection when Login in

--- ProjectX/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ProjectX/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,20 +1,11 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
+﻿#nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using ProjectX.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectX.Areas.Identity.Pages.Account
 {
@@ -116,7 +107,7 @@ namespace ProjectX.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToAction("Index", "Salons");
                 }
                 if (result.RequiresTwoFactor)
                 {
