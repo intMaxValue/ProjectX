da942cb773bb5928664a46685e585ec09bda7897
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-25 12:36:50 +0200

    Added pagination ViewModels

---
+++ ProjectX.ViewModels/Salon/PaginationInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectX.ViewModels.Salon
+{
+    public class PaginationInfoViewModel
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
---
+++ ProjectX.ViewModels/Salon/SalonIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectX.ViewModels.Salon
+{
+    public class SalonIndexViewModel
+    {
+        public IEnumerable<SalonViewModel> Salons { get; set; }
+        public PaginationInfoViewModel PaginationInfo { get; set; }
+    }
+}
