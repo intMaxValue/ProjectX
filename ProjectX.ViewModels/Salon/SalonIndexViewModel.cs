ddd8a6acf8adca50ff7773c3c701d319ebf5f084
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-25 13:01:29 +0200

    Modified ViewModel

--- ProjectX.ViewModels/Salon/SalonIndexViewModel.cs
+++ ProjectX.ViewModels/Salon/SalonIndexViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class SalonIndexViewModel
     {
-        public IEnumerable<SalonViewModel> Salons { get; set; }
-        public PaginationInfoViewModel PaginationInfo { get; set; }
+        public IEnumerable<SalonViewModel> Salons { get; set; } = null!;
+        public PaginationInfoViewModel PaginationInfo { get; set; } = null!;
     }
 }
