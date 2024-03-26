a13df0684331d0761bd0a0bdaefbbfd095d8bbe6
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-26 13:47:58 +0200

    Added data annotation attributes for validation

--- ProjectX.Infrastructure/Data/Models/User.cs
+++ ProjectX.Infrastructure/Data/Models/User.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using ProjectX.Infrastructure.Constants;
 using ProjectX.Infrastructure.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectX.Infrastructure.Data.Models
 {
     public class User : IdentityUser
     {
+        [StringLength(DataConstants.User.FirstNameMaxLength, ErrorMessage = DataConstants.User.FirstNameErrorMessage, MinimumLength = DataConstants.User.FirstNameMinLength)]
         public string FirstName { get; set; } = string.Empty;
+
+        [StringLength(DataConstants.User.LastNameMaxLength, ErrorMessage = DataConstants.User.LastNameErrorMessage, MinimumLength = DataConstants.User.LastNameMinLength)]
         public string LastName { get; set; } = string.Empty;
+
+        [StringLength(DataConstants.User.CityMaxLength, ErrorMessage = DataConstants.User.CityErrorMessage, MinimumLength = DataConstants.User.CityMinLength)]
         public string City { get; set; } = string.Empty;
         public UserType UserType { get; set; }
         public string ProfilePicture { get; set; } = string.Empty;
