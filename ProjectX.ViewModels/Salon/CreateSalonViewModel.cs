d6431330f18332ee7399a8ccfc6b135d56d7f27b
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-26 18:56:11 +0200

    Added Back button

--- ProjectX.ViewModels/Salon/CreateSalonViewModel.cs
+++ ProjectX.ViewModels/Salon/CreateSalonViewModel.cs
@@ -1,11 +1,27 @@
-﻿namespace ProjectX.ViewModels.Salon
+﻿using ProjectX.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectX.ViewModels.Salon
 {
     public class CreateSalonViewModel
     {
+        [StringLength(DataConstants.Salon.NameMaxLength, ErrorMessage = DataConstants.Salon.NameErrorMessage, MinimumLength = DataConstants.Salon.NameMinLength)]
         public string Name { get; set; } = string.Empty;
+
+
+        [StringLength(DataConstants.Salon.AddressMaxLength, ErrorMessage = DataConstants.Salon.AddressErrorMessage, MinimumLength = DataConstants.Salon.AddressMinLength)]
         public string Address { get; set; } = string.Empty;
+
+
+        [StringLength(DataConstants.Salon.CityMaxLength, ErrorMessage = DataConstants.Salon.CityErrorMessage, MinimumLength = DataConstants.Salon.CityMinLength)]
         public string City { get; set; } = string.Empty;
+
+
+        [StringLength(DataConstants.Salon.PhoneNumberMaxLength, ErrorMessage = DataConstants.Salon.PhoneNumberErrorMessage, MinimumLength = DataConstants.Salon.PhoneNumberMinLength)]
         public string PhoneNumber { get; set; } = string.Empty;
+
+
+        [StringLength(DataConstants.Salon.DescriptionMaxLength, ErrorMessage = DataConstants.Salon.DescriptionErrorMessage, MinimumLength = DataConstants.Salon.DescriptionMinLength)]
         public string Description { get; set; } = string.Empty;
         public string MapUrl { get; set; } = string.Empty;
         public string OwnerId { get; set; } = string.Empty;
