0b4e24588e0f2662beb5237abb104c7030c1a0d4
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-27 15:51:17 +0200

    Added MySalonController

---
+++ ProjectX/Controllers/MySalonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectX.Controllers
+{
+    public class MySalonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
