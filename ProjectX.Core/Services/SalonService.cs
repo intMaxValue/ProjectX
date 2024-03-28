ba3cba592c2e8ecb1c15a1bbf0e2023de9395fbc
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-28 17:07:38 +0200

    AddPhotoToSalonAsync added

--- ProjectX.Core/Services/SalonService.cs
+++ ProjectX.Core/Services/SalonService.cs
@@ -89,6 +89,38 @@ namespace ProjectX.Core.Services
             return salon;
         }
 
+        public async Task AddPhotoToSalonAsync(int salonId, string photoUrl, string userId, string caption)
+        {
+            // Find the salon by its ID
+            var salon = await _context.Salons.FindAsync(salonId);
+
+            if (salon == null)
+            {
+                throw new ArgumentException("Salon not found", nameof(salonId));
+            }
+
+            // Ensure that the current user is the owner of the salon
+            if (salon.OwnerId != userId)
+            {
+                throw new UnauthorizedAccessException("You are not authorized to add photos to this salon.");
+            }
+
+            var newPhoto = new Photo
+            {
+                Url = photoUrl,
+                Caption = caption,
+                DateUploaded = DateTime.UtcNow,
+                SalonId = salonId,
+                UserId = userId
+            };
+
+            // Add photo to the context
+            _context.Photos.Add(newPhoto);
+
+            // Save changes to the database
+            await _context.SaveChangesAsync();
+        }
+
         public async Task DeleteSalonAsync(int id)
         {
             var salon = await _context.Salons.FindAsync(id);
