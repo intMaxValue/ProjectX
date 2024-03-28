79a93e77b3685f4ce32f4c8fb3e4d240401479da
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-03-28 10:22:28 +0200

    Added MySalonViewModel

---
+++ ProjectX.ViewModels/Salon/MySalonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX.ViewModels.Salon
+{
+    public class MySalonViewModel
+    {
+        public int SalonId { get; set; }
+        public string SalonName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string MapUrl { get; set; } = string.Empty;
+        public string ProfilePictureUrl { get; set; } = string.Empty;
+        public IEnumerable<string> PhotoUrls { get; set; } = null!;
+    }
+}
