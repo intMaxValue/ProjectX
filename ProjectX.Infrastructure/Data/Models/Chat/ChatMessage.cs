3372f0bcb924284fb84bd4fc9b2a7c67deabaa7c
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-04 15:34:54 +0300

    Added ChatMessage model to db

---
+++ ProjectX.Infrastructure/Data/Models/Chat/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectX.Infrastructure.Data.Models.Chat
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public int ChatRoomId { get; set; }
+        public string SenderId { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime DateAndTime { get; set; }
+        public ChatRoom ChatRoom { get; set; } = null!;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
+
