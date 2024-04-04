6151e43d803d6135e554b27a7c781663a555a57a
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-04 15:33:30 +0300

    Added ChatMessageViewModel

---
+++ ProjectX.ViewModels/Chat/ChatMessageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectX.ViewModels.Chat
+{
+    public class ChatMessageViewModel
+    {
+        public string SenderName { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public int ChatRoomId { get; set; }
+        public string SalonProfilePictureUrl { get; set; } = string.Empty;
+    }
+}
