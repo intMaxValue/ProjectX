fbb8aeb0f148930cbdc3780bd3345884c4662ef2
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-04 15:35:05 +0300

    Added ChatRoom model to db

---
+++ ProjectX.Infrastructure/Data/Models/Chat/ChatRoom.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectX.Infrastructure.Data.Models.Chat
+{
+    public class ChatRoom
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public int SalonId { get; set; }
+        public ICollection<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+
+        public Salon Salon { get; set; } = null!;
+    }
+}
