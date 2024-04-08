25e001fccfcc640a556c53cdc684e12e80db6290
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-08 15:02:12 +0300

    Added AppointmentViewModel

---
+++ ProjectX.ViewModels/Chat/AppointmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectX.ViewModels.Appointment
+{
+    public class AppointmentViewModel
+    {
+        [Required] 
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Date and Time")]
+        public DateTime DateTime { get; set; }
+
+        [StringLength(500)]
+        public string Comment { get; set; } = string.Empty;
+
+        [Required]
+        public int SalonId { get; set; } 
+    }
+}
