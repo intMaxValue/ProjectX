cddddb6f9be1514dfcdd0832348e8300d7566bcb
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-08 13:00:41 +0300

    Added SendMessage in ChatController

--- ProjectX/Controllers/ChatController.cs
+++ ProjectX/Controllers/ChatController.cs
@@ -2,6 +2,7 @@
 using ProjectX.Core.Contracts;
 using ProjectX.ViewModels.Chat;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace ProjectX.Controllers
 {
@@ -39,23 +40,22 @@ namespace ProjectX.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SendMessage(ChatMessageViewModel message, int chatRoomId)
+        public async Task<IActionResult> SendMessage(ChatMessageViewModel message, int salonId)
         {
             if (!ModelState.IsValid)
             {
                 // If the model state is not valid, return to the chat room view with validation errors
-                return RedirectToAction(nameof(Index), new { chatRoomId });
+                return RedirectToAction(nameof(Index), new { salonId });
             }
 
             // Retrieve the sender's user ID from the claims
             string senderId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             // Send the message to the chat room
-            await _chatService.SendMessageAsync(message, chatRoomId, senderId);
+            await _chatService.SendMessageAsync(message, senderId, salonId);
 
             // Redirect to the chat room view
-            return RedirectToAction(nameof(Index), new { salonId = chatRoomId });
+            return RedirectToAction(nameof(Index), new { salonId = salonId });
         }
-
     }
 }
