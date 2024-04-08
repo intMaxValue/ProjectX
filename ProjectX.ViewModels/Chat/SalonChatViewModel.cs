4a5201f6fb8fb298258941bd4a0a98b2a9f2f5ba
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-08 15:02:38 +0300

    Edited SalonChatViewModel to include SalonOwnerId

--- ProjectX.ViewModels/Chat/SalonChatViewModel.cs
+++ ProjectX.ViewModels/Chat/SalonChatViewModel.cs
@@ -4,6 +4,7 @@
     {
         public int SalonId { get; set; }
         public string SalonName { get; set; } = string.Empty;
+        public string SalonOwnerId { get; set; } = string.Empty;
         public string SalonProfilePictureUrl { get; set; } = string.Empty;
         public List<ChatMessageViewModel> Messages { get; set; } = new List<ChatMessageViewModel>();
         public string NewMessage { get; set; } = string.Empty;
