ddf903a39cae6433dd586b1b54ecb6952a3ac37f
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-10 12:38:42 +0300

    Added Seed SalonConfiguration

---
+++ ProjectX.Infrastructure/Data/Seed/SalonConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectX.Infrastructure.Data.Models;
+
+namespace ProjectX.Infrastructure.Data.Seed
+{
+    internal class SalonConfiguration : IEntityTypeConfiguration<Salon>
+    {
+        public void Configure(EntityTypeBuilder<Salon> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new Salon[] { data.SalonSv, data.SalonPl, data.SalonRs, data.SalonVt, data.SalonSf, data.SalonVn, data.SalonSz });
+        }
+    }
+}
