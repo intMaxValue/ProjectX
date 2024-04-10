ee9223c01f3b42351a4e058ab3dfe9496b82de86
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-10 12:39:03 +0300

    Added Seed UserConfiguration

---
+++ ProjectX.Infrastructure/Data/Seed/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectX.Infrastructure.Data.Models;
+
+namespace ProjectX.Infrastructure.Data.Seed
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new User[] { data.Admin, data.User,
+                data.SalonOwnerSv, data.SalonOwnerPl, data.SalonOwnerRs, data.SalonOwnerVt, data.SalonOwnerSf, data.SalonOwnerSz, data.SalonOwnerVn });
+        }
+    }
+}
