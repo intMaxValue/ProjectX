4fedf4575a319fec420d3b3f8b2b35e587cbacf8
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-11 14:58:28 +0300

    Added ErrorController

---
+++ ProjectX/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectX.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet("/Error/{statusCode}")]
+        public IActionResult Error(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return View("404NotFound");
+                case 500:
+                    return View("500InternalServerError");
+                default:
+                    return View("404NotFound");
+            }
+        }
+    }
+}
