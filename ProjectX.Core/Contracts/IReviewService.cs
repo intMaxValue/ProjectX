2302f0e1bd2311a8b146eebc872935498c0a3c55
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-12 12:30:28 +0300

    Added IReviewService

---
+++ ProjectX.Core/Contracts/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿using ProjectX.Infrastructure.Data.Models;
+using ProjectX.ViewModels.Reviews;
+
+namespace ProjectX.Core.Contracts
+{
+    public interface IReviewService
+    {
+        Task<IList<ReviewViewModel>> GetReviewsForSalonAsync(int salonId);
+        Task CreateReviewAsync(ReviewViewModel review);
+        Task<Review?> GetReviewByIdAsync(int id);
+        Task UpdateReviewAsync(ReviewViewModel review);
+        Task DeleteReviewAsync(int id);
+        Task<string> GetSalonProfilePictureAsync(int salonId);
+    }
+}
