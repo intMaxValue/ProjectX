5a3c6b0803f2e90b5c688329c9816e4751059f59
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-12 12:33:07 +0300

    Added ReviewViewModel

---
+++ ProjectX.ViewModels/Reviews/ReviewViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ProjectX.Infrastructure.Constants.DataConstants.Review;
+
+namespace ProjectX.ViewModels.Reviews
+{
+    public class ReviewViewModel
+    {
+        public int Id { get; set; }
+        public int SalonId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string UserCity { get; set; } = string.Empty;
+        public string? UserProfilePictureUrl { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ReviewMaxLength, MinimumLength = ReviewMinLength, ErrorMessage = ReviewErrorMessage)]
+        public string Comment { get; set; } = string.Empty;
+        public DateTime DatePosted { get; set; }
+    }
+}
