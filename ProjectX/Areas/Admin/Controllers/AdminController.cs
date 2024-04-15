52550e843cb70920910bbb21a728ba068c98c504
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-15 20:01:55 +0300

    Added AdminController for admin area

---
+++ ProjectX/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectX.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        
+    }
+}
