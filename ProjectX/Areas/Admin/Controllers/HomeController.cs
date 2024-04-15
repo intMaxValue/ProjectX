e9679397f824ede0063226af754cd16d20335b67
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2024-04-15 20:02:10 +0300

    Added HomeController for admin area

---
+++ ProjectX/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectX.Areas.Admin.Controllers
+{
+    public class HomeController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
